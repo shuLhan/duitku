1cf5da9119f2e23b7de5c2500c3cea521ee6e73e
Shulhan <ms@kilabit.info>
2022-11-07 14:15:25 +0700

    all: add missing field Purpose and UserID in RtolTransferResponse
    While at it, use and embed RtolInquiryResponse inside RtolTransferResponse
    to minimize duplication.

--- rtol_transfer_response.go
+++ rtol_transfer_response.go
@@ -1,15 +1,10 @@
 package duitku
 
-import "github.com/shuLhan/share/lib/math/big"
-
 // RtolTransferResponse contains response from online transfer.
 type RtolTransferResponse struct {
-	Email         string   `json:"email"`
-	BankCode      string   `json:"bankCode"`
-	BankAccount   string   `json:"bankAccount"`
-	AccountName   string   `json:"accountName"`
-	CustRefNumber string   `json:"custRefNumber"`
-	Amount        *big.Rat `json:"amountTransfer"`
+	Purpose string `json:"purpose"`
 
-	Response
+	RtolInquiryResponse
+
+	UserID int64 `json:"userId"`
 }
